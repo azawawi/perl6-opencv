1d86c2a24a46533ee87f5525e636073d03b51cba
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-01-17 12:17:03 +0200

    Require perl 6.c and disable build on windows

--- Build.pm
+++ Build.pm
@@ -5,6 +5,10 @@ use Shell::Command;
 
 class Build is Panda::Builder {
     method build($workdir) {
+        if $*DISTRO.is-win {
+          say "Precompiled binary will be used. Disabling build on windows";
+          return;
+        }
         my $makefiledir = "$workdir/src";
         my $destdir = "$workdir/resources";
         mkpath $destdir;
